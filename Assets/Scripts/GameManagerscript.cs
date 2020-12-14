04ceeeff565b864e6c1269e146c11e58a8efb3a9
Florian Rossignol <rossignol.florian.fr@gmail.com>
2020-12-14 15:48:05 +0100

    adding missing folder

---
+++ Assets/Scripts/GameManagerscript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerscript : MonoBehaviour
+{
+    [SerializeField] GameObject pausingGameobject_;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            StartingPauseMenu();
+        }
+    }
+    private void StartingPauseMenu()
+    { 
+        Debug.Log("lol");
+        Time.timeScale = 0;
+        pausingGameobject_.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnGame()
+    {
+        Time.timeScale = 1;
+        pausingGameobject_.SetActive(false);
+    }
+}
