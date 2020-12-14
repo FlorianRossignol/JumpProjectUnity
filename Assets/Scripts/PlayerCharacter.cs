7aaf6e6948af51ae2f2097e80f18ad5905a30592
Florian Rossignol <rossignol.florian.fr@gmail.com>
2020-12-14 15:40:34 +0100

    adding foot script and working for animation script/ adding a reset function for the player

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.SceneManagement;
 
-public class PlayerCharacter: MonoBehaviour
+public class PlayerCharacter : MonoBehaviour
 {
     public enum State
     {
@@ -11,14 +14,18 @@ public class PlayerCharacter: MonoBehaviour
         WALK,
         JUMP
     }
+    [SerializeField] private PlayerCharacterFootScript foot_;
+    private State currentState_ = State.NONE;
     private const float moveSpeed_ = 2.0f;
-    private const float jumpSpeed_= 3.0f;
+    private const float jumpSpeed_ = 3.0f;
     private float moveDir_ = 0.0f;
     Rigidbody2D body_;
     private Transform transform_;
     bool isFacingRight_ = false;
     SpriteRenderer sprite_;
     Camera cam_;
+    Animator anim_;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +33,8 @@ public class PlayerCharacter: MonoBehaviour
         sprite_ = GetComponent<SpriteRenderer>();
         transform_ = GetComponent<Transform>();
         cam_ = Camera.main;
+        anim_ = GetComponent<Animator>();
+        ChangeState(State.IDLE);
     }
 
     // Update is called once per frame
@@ -38,32 +47,72 @@ public class PlayerCharacter: MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            moveDir_ =- 1.0f;
+            moveDir_ = -1.0f;
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Jump();
         }
-        
+
         var vel = body_.velocity;
-        body_.velocity = new Vector2(moveSpeed_ * moveDir_ , body_.velocity.y);
+        body_.velocity = new Vector2(moveSpeed_ * moveDir_, body_.velocity.y);
+
+        switch (currentState_)
+        {
+            case State.IDLE:
+                if (foot_.FootContact == 0)
+                {
+                    ChangeState(State.JUMP);
+                }
+                break;
+            case State.WALK:
+
+                if (foot_.FootContact == 0)
+                {
+                    ChangeState(State.JUMP);
+                }
+                break;
+            case State.JUMP:
+                if (foot_.FootContact > 0)
+                {
+                    ChangeState(State.IDLE);
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
 
     private void PlayerReset()
     {
-        if(cam_.transform.position.y > body_.transform.position.y +100)
+        if(Input.GetKeyDown(KeyCode.R))
         {
-            cam_.Reset();
-        }
-        if(cam_.transform.position.x > body_.transform.position.x + 100)
-        {
-            cam_.Reset();
+            SceneManager.LoadScene("GameScene");
         }
     }
     private void Jump()
     {
         var vel = body_.velocity;
         body_.velocity = new Vector2(body_.velocity.x, jumpSpeed_);
+    }
+
+    void ChangeState(State state)
+    {
+        switch(state)
+        {
+            case State.IDLE:
+                anim_.Play("idle");
+                break;
+            case State.WALK:
+                anim_.Play("walk");
+                break;
+            case State.JUMP:
+                anim_.Play("jump");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(state), state, null);
+        }
+        currentState_ = state;
     }
 }
