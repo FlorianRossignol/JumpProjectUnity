17bff9d6eed304f99b6c257755862413de325ffa
Florian Rossignol <rossignol.florian.fr@gmail.com>
2021-01-05 14:10:16 +0100

    adding a mooving prefab and script for it

---
+++ Assets/Scripts/Platform/MoovingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoovingPlatform : MonoBehaviour
+{
+    private float Speed_;
+    Rigidbody2D Body_;
+    void Start()
+    {
+        Body_ = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Body_.velocity = Vector2.right * Speed_;
+    }
+}
