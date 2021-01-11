bf7e277099b1262def346dbc467d03a976ca05fb
Florian Rossignol <rossignol.florian.fr@gmail.com>
2021-01-11 14:14:14 +0100

    add an option for the volume of the game

---
+++ Assets/Scripts/Other/SoundFxVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundFxVolume : MonoBehaviour
+{
+    [SerializeField] Slider slider_;
+    [SerializeField] Slider sliderfx_;
+    [SerializeField] AudioSource soundvolume_;
+    [SerializeField] AudioClip walksound_;
+    [SerializeField] AudioClip jumpsound_;
+
+
+    public void VolumeSound()
+    {
+        soundvolume_.volume = slider_.value;
+    }
+
+    public void FxVolume()
+    {
+        soundvolume_.PlayOneShot(walksound_, soundvolume_.volume);
+    }
+}
