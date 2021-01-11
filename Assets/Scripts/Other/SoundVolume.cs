33776bab2591f58646e4ccb9fda7d9dff3ac5664
Florian Rossignol <rossignol.florian.fr@gmail.com>
2021-01-11 16:02:35 +0100

    corringing volume script

---
+++ Assets/Scripts/Other/SoundVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundVolume : MonoBehaviour
+{
+    [SerializeField] Slider slider_;
+    [SerializeField] AudioSource soundvolume_;
+
+
+    public void VolumeSound()
+    {
+        soundvolume_.volume = slider_.value;
+    }
+}
